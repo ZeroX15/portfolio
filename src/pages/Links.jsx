4b6a63dab6195c70bf85d1806e6a5327629fbb58
Marcelo José <77304775+ZeroX15@users.noreply.github.com>
2023-11-20 17:55:35 -0400

    Updated pages

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -1,4 +1,7 @@
 import './Links.css'
+import linkedin from '../assets/linkedin.png'
+import github from "../assets/github.png"
+import discord from "../assets/discord.png"
 const Links = () => {
   return (
     <>
@@ -15,15 +18,15 @@ const Links = () => {
       <div className="Network">
         <h1>Minhas redes sociais: </h1>
         <div className="Nets">
-          <img src="../src/assets/linkedin.png" alt="Linkedin" />
+          <img src={linkedin} alt="Linkedin" />
           <a href='https://www.linkedin.com/in/marcelo-josé-6a4321222/'>Marcelo José</a>
         </div>
         <div className="Nets">
-          <img src="../src/assets/github.png" alt="GitHub" />
+          <img src={github} alt="GitHub" />
           <a href='https://github.com/ZeroX15/'>@ZeroX15</a>
         </div>
         <div className="Nets">
-          <img src="../src/assets/discord.png" alt="Discord" />
+          <img src={discord} alt="Discord" />
           <a href='https://discordapp.com/users/319256076178423810'>@zerox_ap</a>
         </div>
       </div>
